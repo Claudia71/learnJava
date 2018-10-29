62f098274dbde6890577cb8b2345603829a2d60d
Claudia71 <tm71cld@gmail.com>
2018-10-29 18:00:33 +0200

    modificari

--- src/base/CollectionTest.java
+++ src/base/CollectionTest.java
@@ -1,49 +1,50 @@
 package base;
 
-import java.util.ArrayList;
 import java.util.Set;
 import java.util.TreeMap;
 
 public class CollectionTest {
 
-    public static void main(String[]args){
+    public static void main(String[] args) {
 
         //AltaMetoda();
 
-        TreeMap<Integer,String> mapOfString = new TreeMap<>();
+        TreeMap<Integer, String> mapOfString = new TreeMap<>();
 
-        mapOfString.put(2,"second");
-        mapOfString.put(3,"third");
-        mapOfString.put(1,"first");
+        mapOfString.put(2, "second");
+        mapOfString.put(3, "third");
+        mapOfString.put(1, "first");
 
         //modify by key
-        mapOfString.put(1,"newfirst");
+        mapOfString.put(1, "newfirst");
 
         Set<Integer> keys = mapOfString.keySet();
 
 
-        System.out.println("Keys: "+ mapOfString.keySet());
+        System.out.println("Keys: " + mapOfString.keySet());
         System.out.println("values: " + mapOfString.values());
 
-        for (int key:keys){
+        for (int key : keys) {
 
             System.out.println(key + ":" + mapOfString.get(key));
 
-           // keys.forEach(System.out::println);
+            // keys.forEach(System.out::println);
         }
 
     }
 
-    private static void AltaMetoda() {
-        ArrayList<String> listOfStrings = new ArrayList<>();
-
-        listOfStrings.add("first");
-        listOfStrings.add("second");
-        listOfStrings.add("third");
-
-
-        for(int i =0; i<listOfStrings.size(); i++){
-            System.out.println(listOfStrings.get(i));
-        }
-    }
+// --Commented out by Inspection START (29/10/2018 17:50):
+//    private static void AltaMetoda() {
+//        ArrayList<String> listOfStrings = new ArrayList<>();
+//
+//        listOfStrings.add("first");
+//        listOfStrings.add("second");
+//        listOfStrings.add("third");
+//
+//
+//        for (String listOfString : listOfStrings) {
+//            System.out.println(listOfString);
+//        }
+//    }
+// --Commented out by Inspection STOP (29/10/2018 17:50)
 }
